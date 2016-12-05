65fe9523b6d268c10a6605e2764a5bbcd45b9165
kevin-gadek <kgadek@sbcglobal.net>
2016-12-05 14:41:21 -0800

    Initial commit, added log-in screen

---
+++ app.js
@@ -0,0 +1,183 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+ 
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+ 
+server.listen(8080);
+console.log("Server started at port 8080.");
+ 
+var sockets = {};
+ 
+var Entity = function(){
+    var self = {
+        x:250,
+        y:250,
+        spdX:0,
+        spdY:0,
+        id:"",
+    }
+    self.update = function(){
+        self.updatePosition();
+    }
+    self.updatePosition = function(){
+        self.x += self.spdX;
+        self.y += self.spdY;
+    }
+    return self;
+}
+ 
+var Player = function(id){
+    var self = Entity();
+    self.id = id;
+    self.number = "" + Math.floor(10 * Math.random());
+    self.pressingRight = false;
+    self.pressingLeft = false;
+    self.pressingUp = false;
+    self.pressingDown = false;
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+    self.maxSpd = 10;
+   
+    var super_update = self.update;
+    self.update = function(){
+        self.updateSpd();
+        super_update();
+		
+		if(self.pressingAttack){
+			self.shootBullet(self.mouseAngle);
+		}
+    }
+	self.shootBullet = function(angle){
+		var b = Bullet(angle);
+			b.x = self.x;
+			b.y = self.y;
+	}
+   
+   
+    self.updateSpd = function(){
+        if(self.pressingRight)
+            self.spdX = self.maxSpd;
+        else if(self.pressingLeft)
+            self.spdX = -self.maxSpd;
+        else
+            self.spdX = 0;
+       
+        if(self.pressingUp)
+            self.spdY = -self.maxSpd;
+        else if(self.pressingDown)
+            self.spdY = self.maxSpd;
+        else
+            self.spdY = 0;     
+    }
+    Player.list[id] = self;
+    return self;
+}
+Player.list = {};
+Player.onConnect = function(socket){
+    var player = Player(socket.id);
+    socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+		else if(data.inputId === 'attack')
+			player.pressingAttack = data.state;
+		else if(data.inputId === 'mouseAngle')
+			player.mouseAngle = data.state;
+    });
+}
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+}
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        });    
+    }
+    return pack;
+}
+ 
+ 
+var Bullet = function(angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+   
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+Bullet.list = {};
+ 
+Bullet.update = function(){
+ 
+   
+    var pack = [];
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });    
+    }
+    return pack;
+}
+ 
+var io = require('socket.io')(server,{});
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+    sockets[socket.id] = socket;
+	
+   socket.on('signIn', function(data){
+	   if(data.username === 'admin' && data.password === '123'){
+	     Player.onConnect(socket);
+		socket.emit('signInResponse', {success:true});
+	   }else{
+		socket.emit('signInResponse', {success:false});
+	   }
+   })
+   
+   
+
+   
+    socket.on('disconnect',function(){
+        delete sockets[socket.id];
+        Player.onDisconnect(socket);
+    });
+   
+});
+ 
+setInterval(function(){
+    var pack = {
+        player:Player.update(),
+        bullet:Bullet.update(),
+    }
+   
+    for(var i in sockets){
+        var socket = sockets[i];
+        socket.emit('newPositions',pack);
+    }
+},1000/25);
